f22f2918cad24b83cca41bdc6144fbc28862b7e9
agchavez <angelchavez.325.ac@gmail.com>
2023-09-20 21:47:22 -0600

    feat: :sparkles: Mejoras Admin site

--- apps/maintenance/admin.py
+++ apps/maintenance/admin.py
@@ -7,18 +7,53 @@ from apps.maintenance.models.driver import DriverModel
 from apps.maintenance.models.period import PeriodModel
 # Register your models her
 
-admin.site.register(DistributorCenter)
 admin.site.register(RouteModel)
 admin.site.register(LocationModel)
 admin.site.register(OperatorModel)
-admin.site.register(TransporterModel)
-admin.site.register(TrailerModel)
-admin.site.register(ProductModel)
-admin.site.register(DriverModel)
 admin.site.register(OutputTypeModel)
-admin.site.register(PeriodModel)
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ['name']
+    list_filter = ['brand', 'created_at']
+    # ordering = ('id', 'name', 'description', 'is_active', 'created_at', 'updated_at')
+
+
+class DistributorCenterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ['name']
+
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'created_at')
+    search_fields = ['first_name', 'last_name']
+    list_filter = ['created_at']
+
+
+class TrailerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code')
+    search_fields = ['code']
+
+
+class TransporterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'tractor')
+    search_fields = ['code', 'name']
+    list_filter = ['created_at']
+
+
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'label', 'initialDate', 'distributor_center')
+    search_fields = ['label', 'initialDate']
+    list_filter = ['created_at', 'label', 'distributor_center', 'initialDate']
+
+
+admin.site.register(TransporterModel, TransporterAdmin)
+admin.site.register(PeriodModel, PeriodAdmin)
+admin.site.register(TrailerModel, TrailerAdmin)
+admin.site.register(DriverModel, DriverAdmin)
+admin.site.register(ProductModel, ProductAdmin)
+admin.site.register(DistributorCenter, DistributorCenterAdmin)
 # Titulo del Panel de Administracion
 admin.site.site_header = 'Administracion de la Aplicacion'
 # Titulo de la pagina de Administracion
