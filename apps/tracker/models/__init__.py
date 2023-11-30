b005228caffb7f3341672a73767f40dc9acbe33f
agchavez <angel.chavez.325.ac@gmail.com>
2023-11-30 16:41:01 -0600

    feat: :sparkles: inizializar t2

--- apps/tracker/models/__init__.py
+++ apps/tracker/models/__init__.py
@@ -1,2 +1,3 @@
 from .typeDetailOutput import TypeDetailOutputModel
 from .tracker import TrackerModel, TrackerDetailModel, TrackerDetailProductModel, TrackerDetailOutputModel
+from .tracker_t2 import OutputT2Model, OutputDetailT2Model, TrackerOutputT2Model
