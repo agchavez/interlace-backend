0c7b3e67bfd097d6ea2166f5e22a173b0486dba9
agchavez <angelchavez.325.ac@gmail.com>
2024-02-08 06:03:10 -0600

    fix: :bug: actualizacion de requerimientos

--- apps/maintenance/views/period.py
+++ apps/maintenance/views/period.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, mixins
-from ..models import PeriodModel, DistributorCenter
+from ..models import PeriodModel, DistributorCenter, ProductModel
 from ..serializer import PeriodModelSerializer, DistributorCenterSerializer
 from apps.user.models import UserModel
 from apps.user.serializers import UserSerializer
@@ -10,6 +10,8 @@ from apps.user.views.user import CustomAccessPermission
 from rest_framework.exceptions import APIException
 from apps.maintenance.exceptions.maintenance import NoProductError, ProductNoIntegerError
 from datetime import date
+import pandas as pd
+
 class PeriodViewSet(mixins.ListModelMixin, 
                     mixins.RetrieveModelMixin, 
                     viewsets.GenericViewSet):
@@ -17,12 +19,16 @@ class PeriodViewSet(mixins.ListModelMixin,
     serializer_class = PeriodModelSerializer
     permission_classes = [CustomAccessPermission]
     PERMISSION_MAPPING = {
-        'GET': ['period.view_periodmodel'],
-        'POST': ['period.add_periodmodel'],
-        'PUT': ['period.change_periodmodel'],
-        'PATCH': ['period.change_periodmodel'],
-        'DELETE': ['period.delete_periodmodel'],
+        'GET': [],
+        'POST': [],
+        'PUT': [],
+        'PATCH': [],
+        'DELETE': [],
     }
+
+    def get_required_permissions(self, http_method):
+        return self.PERMISSION_MAPPING.get(http_method, [])
+
 
     @action(detail=False, methods=['get'], url_path='last-period')
     def get_last_period(self, request, *args, **kwargs):
@@ -46,4 +52,32 @@ class PeriodViewSet(mixins.ListModelMixin,
             period = periods[0]
         serializer = self.get_serializer(period)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # carga masiva de periodos
+    @action(detail=False, methods=['post'], url_path='load-excel')
+    def load_excel(self, request):
+        file = request.data.get('file')
+        if file is None:
+            raise APIException("No file found")
+        df = pd.read_excel(file)
+        # tiene que existir las columnas: centro_distribucion, producto, fecha_inicial, label
+        if 'centro_distribucion' not in df.columns or 'producto' not in df.columns or 'fecha_inicial' not in df.columns or 'label' not in df.columns:
+            raise APIException("Columns not found")
+        for index, row in df.iterrows():
+            distributor_center = row['centro_distribucion']
+            product = row['producto']
+            initialDate = row['fecha_inicial']
+            label = row['label']
+            if ProductModel.objects.filter(id=product).count() <= 0:
+                continue
+            PeriodModel.objects.create(
+                distributor_center_id=distributor_center,
+                product_id=product,
+                initialDate=initialDate,
+                label=label
+            )
+
+        return Response({
+            'message': 'File loaded'
+        }, status=status.HTTP_201_CREATED)
  
