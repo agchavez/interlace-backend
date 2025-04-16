ee10583ed53eae629ea5df976baf61c110742509
agchavez <angel.chavez.325.ac@gmail.com>
2025-04-15 22:57:19 -0600

    feat: :sparkles: create new storage_backends.py for AzureMediaStorage implementation; move class definition from __init__.py

--- apps/core/__init__.py
+++ apps/core/__init__.py
@@ -1,7 +1,2 @@
 # apps/core/storage_backends.py
 
-from storages.backends.azure_storage import AzureStorage
-
-class AzureMediaStorage(AzureStorage):
-    azure_container = "tracker"
-    expiration_secs = None  # o un número si querés que los enlaces expiren
